d2d55f31cadb6095fdf898e8a87111dabc385cd8
bilal <bilalezzaa1@gmail.com>
2022-05-13 13:37:31 +0200

    Nettoyage du code

--- src/components/ui/stars/StarsList.js
+++ src/components/ui/stars/StarsList.js
@@ -20,20 +20,6 @@ const StarsList = (props) => {
       starsTotal.push(<Stars key={i} image={starsColored} />);
     }
   }
-  // } else if (number === 0) {
-  //   console.log("0");
-  //   for (let i = 0; i < 5; i++) {
-  //     starsTotal.push(<Stars key={i} image={starsGrey} />);
-  //   }
-  // }
-
-  // if (number > 0 && starsTotal.length !== 5) {
-  //   console.log("Ok");
-  //   // const target = starsTotal.length - 1;
-  //   // for (let i = target; target < 5; i++) {
-  //   //   starsTotal.push(<Stars key={i} image={starsGrey} />);
-  //   // }
-  // }
 
   return starsTotal;
 };
